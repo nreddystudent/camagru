d13ede7895c94a39f15f6f4f1523736b106beddf
nreddystudent <nreddy@student.wethinkcode.co.za>
2019-10-28 11:07:33 +0200

    added mvc

--- register.php
+++ register.php
@@ -1,8 +1,42 @@
-<?php
-    echo $_POST['uname']."<br>";
-    echo $_POST['fname']."<br>";
-    echo $_POST['lname']."<br>";
-    echo $_POST['passw'];
-    echo $_POST['email'];
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Register</title>
+	<link rel="stylesheet" href="login.css">
+</head>
+<body>
+	<div class="box">
+		<h2>Login</h2>
+		<form action="register.php" method="POST">
+			<div class="inputBox">
+				<input type="text" name="fname" required="">
+				<label>First Name</label>
+			</div> 
+			<div class="inputBox">
+				<input type="text" name="lname" required="">
+				<label>Last Name</label>
+			</div> 
+			<div class="inputBox">
+				<input type="email" name="email" required="">
+				<label>Email</label>
+			</div> 
+			<div class="inputBox">
+				 <input type="text" name="uname" required="">
+				 <label>Username</label>
+			</div>
+			<div class="inputBox">
+				 <input type="password" name="passw" required="">
+				 <label>Password</label>
+			</div> 
+			<div class="inputBox">
+				 <input type="password" name="passwc" required="">
+				 <label>Confirm Password</label>
+			</div> 
+			<input type="submit" value="Submit">
+		</form>
+	</div>
+</body>
+</html>
