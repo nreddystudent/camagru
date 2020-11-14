182a9ab078e2d618f704b6e0c916d74eb4a7878a
nreddystudent <nreddy@student.wethinkcode.co.za>
2020-11-14 11:43:36 +0200

    Changed DB password and added instructions

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
     $DB_DSN = 'mysql:host=localhost';
     $DB_USER = 'root';
-    $DB_PASSWORD = 'ichigo0808';
+    $DB_PASSWORD = 'mysql';
     $DB_NAME = 'myshit';
     $DB_DSNF = "mysql:dbname=$DB_NAME;charset=utf8mb4;host=localhost"
 ?>
